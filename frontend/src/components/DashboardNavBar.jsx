8724ba5e34ccab9d7fd6b27fbcc6ec5a13fefb9d
David <dchoi2003@gmail.com>
2025-07-11 16:23:00 +1000

    Styled Dashboard Header

--- frontend/src/components/DashboardNavBar.jsx
+++ frontend/src/components/DashboardNavBar.jsx
@@ -21,17 +21,19 @@ export function DashboardNavBar({ onMenuClick }) {
   const { signOut } = UserAuth();
 
   return (
-    <Navbar fluid rounded>
-      <LuMenu className="w-10 h-10 ml-4" onClick={onMenuClick}/>
+    <Navbar fluid className="bg-white dark: bg-gradient-to-l from-sky-500 to-indigo-600 h-20">
+      <LuMenu className="w-10 h-10 ml-4 mb-4 text-white" onClick={onMenuClick}/>
       <NavbarBrand>
-        <img src={logo} className="mt-1 h-18 w-18"/>
-        <span className="self-center whitespace-nowrap text-4xl font-semibold mb-2">Univise</span>
+        <img src={logo} className="h-18 w-18 mt-1"/>
+        <span className="self-center whitespace-nowrap text-4xl text-white font-semibold mb-2">Univise</span>
       </NavbarBrand>
       <Dropdown
         arrowIcon={false}
         inline
         label={
-          <Avatar alt="User settings" img="https://flowbite.com/docs/images/people/profile-picture-5.jpg" rounded className="mr-4 mb-2"/>
+          <Avatar alt="User settings"  
+        rounded 
+        className="mr-4 mb-3"/>
         }
       >
         <DropdownHeader>
