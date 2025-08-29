a8d9120fe851bb094bce15344ce8b54750e65349
David <dchoi2003@gmail.com>
2025-08-29 13:52:32 +1000

    Fixed the layout of the login page - No more scrolling

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -8,8 +8,8 @@ export function Header() {
     <div className="mb-4">
       <Navbar fluid >
         <NavbarBrand href="https://flowbite-react.com" className="flex items-center">
-          <img src={logo} alt="Univise Logo" className="mt-3"/>
-          <span className="self-center whitespace-nowrap text-5xl font-semibold dark:text-sky-950">Univise</span>
+          <img src={logo} alt="Univise Logo"/>
+          <span className="self-center whitespace-nowrap text-5xl font-semibold dark:text-sky-950 mb-3">Univise</span>
         </NavbarBrand>
         <NavbarToggle />
       </Navbar>
