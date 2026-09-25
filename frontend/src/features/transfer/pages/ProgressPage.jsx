ccf577d69ad1f5346c55bb63c0743ae8f79f392d
Thenuja Wijesuriya <t.wijesuriya@student.unsw.edu.au>
2026-09-25 18:31:43 +1000

    Keep user_id in compare request until backend deploys

--- frontend/src/features/transfer/pages/ProgressPage.jsx
+++ frontend/src/features/transfer/pages/ProgressPage.jsx
@@ -366,6 +366,7 @@ function ProgressPage() {
 
     try {
       const comparisonRequest = {
+        user_id: session.user.id,
         base_program_code: enrolledProgram.degree_code,
         base_specialisation_codes: baseSelectedSpecs,
         target_program_code: targetProgram.code,
